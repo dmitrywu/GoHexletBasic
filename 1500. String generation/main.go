c86c2aa188b0807f2840d7728cde652508d9b852
Dmitry Utropov <dmitry.wu@gmail.com>
2025-10-16 08:24:40 +0300

    15 wtf

--- 1500. String generation/main.go
+++ 1500. String generation/main.go
@@ -1,1 +1,33 @@
-﻿
+﻿package main
+
+import (
+	"fmt"
+	//"strings"
+	//	"unicode"
+)
+
+func FilterString(text string, symb rune) string {
+	if text == "" {
+		return text
+	}
+
+	// var sb strings.Builder
+
+	// var r1 byte = byte(unicode.ToLower(symb))
+	// var r2 byte = byte(unicode.ToUpper(symb))
+
+	for _, i := range text {
+		fmt.Print(i)
+		fmt.Println(text)
+
+	}
+
+	return ""
+
+}
+
+func main() {
+
+	fmt.Println(FilterString("If I look forward I win", 'i'))
+
+}
