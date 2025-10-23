60d1b8abda77dd1c7dea418d178330ba3d3e0dd7
Dmitry Utropov <dmitry.wu@gmail.com>
2025-10-23 07:28:01 +0300

    21 wtf

--- 2100. Lambda function/main.go
+++ 2100. Lambda function/main.go
@@ -1,5 +1,21 @@
-﻿package main
+package main
+
+import "fmt"
+
+// MakeGreeting принимает приветствие и возвращает новую функцию,
+// которая добавляет имя к этому приветствию.
+func MakeGreeting(greeting string) func(string) string {
+	return func(name string) string {
+		return fmt.Sprintf("%s, %s!", greeting, name)
+	}
+}
 
 func main() {
+	var m int
+	fmt.Println(m)
+	greeter := MakeGreeting("Hello")
+	fmt.Println(greeter("Hexlet")) // Hello, Hexlet!
 
+	hiGreeter := MakeGreeting("Hi")
+	fmt.Println(hiGreeter("Go")) // Hi, Go!
 }
