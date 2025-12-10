53992d427f02f9fda27339e5a5069c0d4ddb37e7
Dmitry Utropov <dmitry.wu@gmail.com>
2025-12-10 04:39:15 +0300

    24

---
+++ 2200. Arrays/main.go
@@ -0,0 +1,38 @@
+﻿package main
+
+import (
+	"fmt"
+)
+
+func reverse(text [5]rune) [5]rune {
+	var result [5]rune
+	for i := 0; i < len(text); i++ {
+		result[i] = text[len(text)-1-i]
+	}
+	return result
+}
+
+func SafeWrite(nums [5]int, i, val int) [5]int {
+	if i >= 0 && i < len(nums) {
+		nums[i] = val
+		return nums
+	}
+	return nums
+}
+
+func main() {
+	// Массив - фиксированное количество элементов ОДНОГО типа
+	var nums [5]int
+	nms := [5]int{}             // в этом случае скобки обязательны
+	bms := [5]int{1, 2}         // оставшиеся инициализируются 0-ми
+	xms := [...]int{10, 20, 30} // при использовании "..." длина посчитается автоматически
+
+	fmt.Println(nums, nms, bms, xms, len(xms))
+
+	// в функции массивы передаются по значению
+
+	text := [5]rune{'п', 'р', 'и', 'в', 'е'}
+	reversed := reverse(text)
+	fmt.Println(string(reversed[:]))
+
+}
---
+++ 2300. Slices/main.go
@@ -0,0 +1,24 @@
+﻿package main
+
+func GetWorkHours(schedule []int) []int {
+	if len(schedule) < 5 {
+		return []int{}
+	}
+	sum := make([]int, 5)
+	// for _, v := range(schedule[:5]){
+	// 	sum =+v
+	// }
+	copy(sum, schedule)
+	return sum
+}
+
+func main() {
+	var num []int
+	numbers := []int{}
+	numbers1 := []int{10, 10, 30}
+	nums := make([]int, 5)
+	buf := make([]int, 0, 1000)
+	nums = append(nums, 3)
+	nums = append(nums, 2, 1, 0)
+
+}
