55348434371d5488162db67706e9f8e1cc012c91
Dmitry Utropov <dmitry.wu@gmail.com>
2025-12-15 04:39:23 +0300

    27

--- 2600. Slice compare/main.go
+++ 2600. Slice compare/main.go
@@ -5,6 +5,29 @@ import (
 	"slices"
 )
 
+func AreOrderHistoriesEqual(history1, history2 [][]string) bool {
+	if len(history1) != len(history2) {
+		return false
+	}
+
+	if history1 == nil || history2 == nil {
+		return false
+	}
+
+	for i := range history1 {
+		if len(history1[i]) != len(history2[i]) {
+			return false
+		}
+
+		for j := range history1[i] {
+			if history1[i][j] != history2[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func main() {
 	a := []int{1, 2, 3}
 	b := []int{1, 2, 3}
@@ -21,4 +44,14 @@ func main() {
 	empty := []int{}
 	fmt.Println(empty == nil)
 
+	h1 := [][]string{
+		{"milk", "bread"},
+		{"apple", "banana"}}
+
+	h2 := [][]string{
+		{"milk", "bread", "butter"},
+		{"apple", "banana"}}
+
+	fmt.Println("----")
+	fmt.Println(AreOrderHistoriesEqual(h1, h2))
 }
---
+++ 2800. Multiple slice operations/go.mod
@@ -0,0 +1,3 @@
+module github.com/dmitrywu/GoHexletBasic
+
+go 1.25.5
